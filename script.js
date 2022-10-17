e4bcc95b486aa4b9ff575b09984c655d968b6841
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-10-17 10:47:26 -0700

    Pressing "Sign in" console logs usern and pass

--- script.js
+++ script.js
@@ -31,8 +31,16 @@ let password = "admin";
 
 // http://dropper-dev.us-west-1.elasticbeanstalk.com/
 
-doRequest('POST', 'http://dropper-dev.us-west-1.elasticbeanstalk.com/api/login', `user=${username}&pass=${password}`)
+// doRequest('POST', 'http://dropper-dev.us-west-1.elasticbeanstalk.com/api/login', `user=${username}&pass=${password}`)
 
-.then((res) => {
-    console.log(`Session was created. Response from server: ${JSON.stringify(res)}`)
-})
+// .then((res) => {
+//     console.log(`Session was created. Response from server: ${JSON.stringify(res)}`)
+// })
+
+setUserAndPass = () => {
+    user = document.getElementById('email-address').value;
+    pass = document.getElementById('password').value;
+    console.log(user);
+    console.log(pass);
+
+};
