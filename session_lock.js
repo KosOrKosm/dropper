a5aaa542bfc6afc6481c7e4b69526c52b1c5b1e1
Jacob Fano <jacobfano7@csu.fullerton.edu>
2022-10-18 13:53:42 -0700

    Fix purging old sessions in session lock

--- session_lock.js
+++ session_lock.js
@@ -10,19 +10,18 @@ function unixnow() {
     return Math.floor(Date.now() / 1000)
 }
 
-async function purgeOldSessions(user) {
+async function purgeOldSessions() {
+    console.log(`Purging sessions from before: ${unixnow()}`)
     var result = await db.scan({
         TableName: 'sessions',
-        FilterExpression: "#user-id = :user AND #timeout <= :now",
+        FilterExpression: "#timeout <= :now",
         ExpressionAttributeNames: {
-            '#user-id': 'user-id',
             '#timeout': 'timeout'
         },
         ExpressionAttributeValues: {
-            ':user': user,
             ':now': unixnow()
         }
-    })
+    }).promise()
     
     const batch = {
         'sessions': []
@@ -35,44 +34,49 @@ async function purgeOldSessions(user) {
         })
     }
     
-    await db.batchWrite(batch)
+    await db.batchWrite({
+        RequestItems: batch
+    }).promise()
 
 }
 
 router.use((req, res, next) => {
 
-    console.log("Entering session lock check")
-    const session = req.cookies.session
-    if (session == undefined) {
-        res.status(401).json("No session found. Please login.")
-        return
-    }
+    purgeOldSessions()
+    .then(() => {
 
-    db.get({
-        TableName: 'sessions',
-        Key: { 'session-id': session }
-    }).promise()
-    .then((result) => {
-        var inTable = result.Item
-        if (inTable != undefined) {
-            // Purge old sessions. TODO: do this somewhere more sensible
-            // purgeOldSessions(inTable['user-id'])
-            if (inTable.timeout > unixnow()) {
-                // set the user of the current session for future reference
-                req.user = inTable['user-id'] 
-                next()
-            } else {
-                res.status(401).json("Session expired. Please login.")
-            }
-        } else {
+        console.log("Entering session lock check")
+        const session = req.cookies.session
+        if (session == undefined) {
             res.status(401).json("No session found. Please login.")
+            return
         }
+
+        db.get({
+            TableName: 'sessions',
+            Key: { 'session-id': session }
+        }).promise()
+        .then((result) => {
+            var inTable = result.Item
+            if (inTable != undefined) {
+                // Purge old sessions. TODO: do this somewhere more sensible
+                if (inTable.timeout > unixnow()) {
+                    // set the user of the current session for future reference
+                    req.user = inTable['user-id'] 
+                    next()
+                } else {
+                    res.status(401).json("Session expired. Please login.")
+                }
+            } else {
+                res.status(401).json("No session found. Please login.")
+            }
+        })
+        .catch((error) => {
+            // TODO: do not send raw errors to client
+            res.status(500).json(error)
+        })
     })
-    .catch((error) => {
-        // TODO: do not send raw errors to client
-        res.status(500).json(error)
-    })
-    
+
 })
 
 module.exports = router
