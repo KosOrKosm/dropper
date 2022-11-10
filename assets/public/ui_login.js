61d20989a6793e0b8e3ff74db9839f5e3aabeb37
Jacob Fano <jacobfano7@csu.fullerton.edu>
2022-11-10 09:13:44 -0800

    Move common UI code to seperate file

--- assets/public/ui_login.js
+++ assets/public/ui_login.js
@@ -1,30 +1,4 @@
 
-function doRequest(method, URL, body) {
-    return new Promise(function (resolve, reject) {
-        const xhr = new XMLHttpRequest()
-        xhr.open(method, URL)
-        if(body != null && body != undefined)
-            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response)
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                })
-            }
-        }
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            })
-        }
-        xhr.send(body)
-    })
-}
-
 window.addEventListener('load', (ev) => {
 
     document.getElementById('btn-signin').addEventListener('click', (ev) => {
