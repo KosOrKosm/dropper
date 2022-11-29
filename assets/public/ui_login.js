1c6f97f415c5cb5dde554e7f67d1aa6648a0dc1c
Jacob Fano <jacobfano7@csu.fullerton.edu>
2022-11-28 19:05:46 -0800

    Change login notifcation to be inline

--- assets/public/ui_login.js
+++ assets/public/ui_login.js
@@ -8,7 +8,6 @@ window.addEventListener('load', (ev) => {
 
         let user_in = document.getElementById('input-username')
         let pass_in = document.getElementById('input-password')
-        let popup = document.getElementById("popup")
         var api_root = '../api'
         
         // Submit the login
@@ -17,23 +16,26 @@ window.addEventListener('load', (ev) => {
             `${api_root}/login`, 
             `user=${user_in.value}&pass=${pass_in.value}`
         ).then((res) => {
-            // TODO: show a popup indicating success
+            let notifier = document.getElementById("login_notification")
+            notifier.innerText = 'Login Successful!'
+            notifier.style.setProperty('background-color', 'darkgreen')
+            notifier.className = notifier.className.replace('hidden', 'active')
+            setTimeout(() => {
+                notifier.className = notifier.className.replace('active', 'hidden')
+            }, 5000)
         }).catch((err) => {
-            // TODO: show a popup indicating failure
-            alert('Failed to login! Try again.')
+            let notifier = document.getElementById("login_notification")
+            notifier.innerText = 'Invalid credentials! Try again.'
+            notifier.style.setProperty('background-color', 'red')
+            notifier.className = notifier.className.replace('hidden', 'active')
+            setTimeout(() => {
+                notifier.className = notifier.className.replace('active', 'hidden')
+            }, 5000)
         })
 
         // Reset fields
         user_in.value = ''
         pass_in.value = ''
-
-        // Popup Functionality - Incorrect User/Password
-        function openPopup() {
-            popup.classList.add("open-popup");
-        }
-        function closePopup() {
-            popup.classList.remove("open-popup");
-        }
     })
 
 })
